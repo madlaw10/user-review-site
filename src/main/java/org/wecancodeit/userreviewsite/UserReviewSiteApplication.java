cfec7023564c05d43f46b278df8f85bf9e447144
Madison Lawton <madison.e.lawton@gmail.com>
2019-02-18 14:10:48 -0500

    create repository

---
+++ src/main/java/org/wecancodeit/userreviewsite/UserReviewSiteApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.userreviewsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserReviewSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserReviewSiteApplication.class, args);
+	}
+
+}
