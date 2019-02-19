ff3a08ff5ddf00986be4d7646b0dcf656cfec0cd
Madison Lawton <madison.e.lawton@gmail.com>
2019-02-19 11:15:09 -0500

    basic models and controller

--- src/test/java/org/wecancodeit/userreviewsite/ReviewTest.java
+++ src/test/java/org/wecancodeit/userreviewsite/ReviewTest.java
@@ -4,21 +4,20 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-
-
 public class ReviewTest {
-Review underTest;
-@Before
-public void setup() {
-	underTest = new Review(1, "title",  null, "category", "content");
-}
-@Test
-public void testEditReviewContent(){
-	String newReview = "edited content";
-	underTest.edit(newReview);
-	
-	Assert.assertEquals("edited content", underTest.getContent());
-	
-	
-}
+	Review underTest;
+
+	@Before
+	public void setup() {
+		underTest = new Review(1, "title", 0, "author", null, "category", "content");
+	}
+
+	@Test
+	public void testEditReviewContent() {
+		String newReview = "edited content";
+		underTest.edit(newReview);
+
+		Assert.assertEquals("edited content", underTest.getContent());
+
+	}
 }
