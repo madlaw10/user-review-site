5f051e6d6312e9c0cf5a0352dfab3c063fb09ebd
tulasi <tulasipinjala.com>
2019-02-19 12:45:49 -0500

    fix merged

--- src/test/java/org/wecancodeit/userreviewsite/ReviewTest.java
+++ src/test/java/org/wecancodeit/userreviewsite/ReviewTest.java
@@ -5,22 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ReviewTest {
-<<<<<<< HEAD
-Review underTest;
-@Before
-public void setup() {
-	underTest = new Review(1, "title",  null, "category", "content");
-}
-@Test
-public void testEditReviewContent(){
-	String newReview = "edited content";
-	underTest.edit(newReview);
-	
-	Assert.assertEquals("edited content", underTest.getContent());
-	
-	
-}
-=======
+
 	Review underTest;
 
 	@Before
@@ -36,5 +21,4 @@ public void testEditReviewContent(){
 		Assert.assertEquals("edited content", underTest.getContent());
 
 	}
->>>>>>> f9909e73152a9ba6e81022c00cee3ed95ad7a48b
 }
