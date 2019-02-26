dbda6e40c8d15a7b7b77d4174b8e482b7cb21a55
sinogue <eclypso@aol.com>
2019-02-25 23:03:12 -0500

    CSS changes, cleaned up HTML, replaced homepage logo

--- src/main/java/org/wecancodeit/userreviewsite/controllers/HomeController.java
+++ src/main/java/org/wecancodeit/userreviewsite/controllers/HomeController.java
@@ -6,17 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.wecancodeit.userreviewsite.repositories.CategoryRepository;
+//import org.wecancodeit.userreviewsite.repositories.CategoryRepository;
 
 @Controller
 public class HomeController {
 	
-	@Resource
-	CategoryRepository categoryRepo;
+//	@Resource
+//	CategoryRepository categoryRepo;
 
 	@GetMapping("/")
 	public String getCategories(Model model) {
-		model.addAttribute("categories", categoryRepo.findAll());
+//		model.addAttribute("categories", categoryRepo.findAll());
 		return "home";
 	}
 
@@ -27,7 +27,7 @@ public class HomeController {
 	
 	@GetMapping("/{tag}")
 	public String getCategory(@PathVariable String tag, Model model) {
-		model.addAttribute("category", categoryRepo.findCategoryByTag(tag));
+//		model.addAttribute("category", categoryRepo.findCategoryByTag(tag));
 		return "category";
 	}
 	
