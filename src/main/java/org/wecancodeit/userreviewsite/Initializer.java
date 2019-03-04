a621ea91eb415608d029a0324b894cc3078eea33
mateofrito <mattfry@outlook.com>
2019-03-03 20:36:28 -0500

    I failed!

--- src/main/java/org/wecancodeit/userreviewsite/Initializer.java
+++ src/main/java/org/wecancodeit/userreviewsite/Initializer.java
@@ -27,99 +27,118 @@ public class Initializer implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		ReviewTag tagTest = reviewTagRepo.save(new ReviewTag("testing"));
 		Category categoryOne = categoryRepo.save(new Category("Amusement & Entertainment"));
 		Review reviewOne = reviewRepo.save(new Review("Riding a Roller Coaster", 5, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", 
 				"madlaw10", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae cupiditate tempore deserunt odit" + 
 						" provident dolore architecto eius et quia temporibus harum ratione quisquam excepturi totam repellat," + 
 						" consequuntur assumenda debitis velit!", 
-						categoryOne));
+						categoryOne, tagTest));
 		Review reviewTwo = reviewRepo.save(new Review("Going to a Music Festival", 2, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", 
 				"madlaw10", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae cupiditate tempore deserunt odit" + 
 						" provident dolore architecto eius et quia temporibus harum ratione quisquam excepturi totam repellat," + 
 						" consequuntur assumenda debitis velit!", 
-						categoryOne));
+						categoryOne, tagTest));
 		
 		Category categoryTwo = categoryRepo.save(new Category("Financial"));
 		Review reviewThree = reviewRepo.save(new Review("Opening a Bank Account", 3, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", 
 				"madlaw10", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae cupiditate tempore deserunt odit" + 
 						" provident dolore architecto eius et quia temporibus harum ratione quisquam excepturi totam repellat," + 
 						" consequuntur assumenda debitis velit!", 
-						categoryTwo));
+						categoryTwo, tagTest));
 		Review reviewFour = reviewRepo.save(new Review("Burying Your Gold", 5, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", 
 				"madlaw10", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae cupiditate tempore deserunt odit" + 
 						" provident dolore architecto eius et quia temporibus harum ratione quisquam excepturi totam repellat," + 
 						" consequuntur assumenda debitis velit!", 
-						categoryTwo));
+						categoryTwo, tagTest));
 		
 		Category categoryThree = categoryRepo.save(new Category("Health & Wellbeing"));
 		Review reviewFive = reviewRepo.save(new Review("Jogging", 3, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", 
 				"madlaw10", "The endorphins are cool and all, but at what cost. Prepare yourself to be bored out of your skull \r\n" + 
 						"the entire time. Create a playlist to distract yourself, or self-administer a lobotomy so you just don't care.", 
-						categoryThree));
+						categoryThree, tagTest));
 		Review reviewSix = reviewRepo.save(new Review("Flossing", 1, "https://images.medicaldaily.com/sites/medicaldaily.com/files/styles/headline/public/2015/06/27/flossing.jpg", 
 				"madlaw10", "Did it one time and my gums were spewing blood like a geyser. I'm never flossing again.", 
-						categoryThree));
+						categoryThree, tagTest));
 		
 		Category categoryFour = categoryRepo.save(new Category("Legal"));
 		Review reviewSeven = reviewRepo.save(new Review("Recouping Your Safety Deposit", 5, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", 
 				"madlaw10", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae cupiditate tempore deserunt odit" + 
 						" provident dolore architecto eius et quia temporibus harum ratione quisquam excepturi totam repellat," + 
 						" consequuntur assumenda debitis velit!", 
-						categoryFour));
+						categoryFour, tagTest));
 		Review reviewFifteen = reviewRepo.save(new Review("Getting Divorced", 5, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", 
 				"madlaw10", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae cupiditate tempore deserunt odit" + 
 						" provident dolore architecto eius et quia temporibus harum ratione quisquam excepturi totam repellat," + 
 						" consequuntur assumenda debitis velit!", 
-						categoryFour));
+						categoryFour, tagTest));
 		
 		Category categoryFive = categoryRepo.save(new Category("Life-Threatening"));
 		Review reviewEight = reviewRepo.save(new Review("Skydiving", 0, "https://www.skydivelongisland.com/images/article/Skydive-Tracking.jpg", 
 				"madlaw10", "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
 						+ "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
 						+ "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH", 
-						categoryFive));
+						categoryFive, tagTest));
 		Review reviewNine = reviewRepo.save(new Review("Foregoing Vaccinations", 0, "https://www.skydivelongisland.com/images/article/Skydive-Tracking.jpg", 
 				"madlaw10", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae cupiditate tempore deserunt odit" + 
 						" provident dolore architecto eius et quia temporibus harum ratione quisquam excepturi totam repellat," + 
 						" consequuntur assumenda debitis velit!",
-						categoryFive));
+						categoryFive, tagTest));
 		
 		Category categorySix = categoryRepo.save(new Category("Professional"));
 		Review reviewTen = reviewRepo.save(new Review("Getting Fired", 1, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", 
 				"madlaw10", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae cupiditate tempore deserunt odit" + 
 						" provident dolore architecto eius et quia temporibus harum ratione quisquam excepturi totam repellat," + 
 						" consequuntur assumenda debitis velit!", 
-						categorySix));
+						categorySix, tagTest));
 		Review reviewEleven = reviewRepo.save(new Review("Quitting", 5, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", 
 				"madlaw10", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae cupiditate tempore deserunt odit" + 
 						" provident dolore architecto eius et quia temporibus harum ratione quisquam excepturi totam repellat," + 
 						" consequuntur assumenda debitis velit!", 
-						categorySix));
+						categorySix, tagTest));
 		Review reviewTwelve = reviewRepo.save(new Review("Negotiating a Raise", 3, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", 
 				"madlaw10", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae cupiditate tempore deserunt odit" + 
 						" provident dolore architecto eius et quia temporibus harum ratione quisquam excepturi totam repellat," + 
 						" consequuntur assumenda debitis velit!", 
-						categorySix));
+						categorySix, tagTest));
 		
 		Category categorySeven = categoryRepo.save(new Category("Vehicular"));
 		Review reviewThirteen = reviewRepo.save(new Review("Sitting in Traffic", 1, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", 
 				"madlaw10", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae cupiditate tempore deserunt odit" + 
 						" provident dolore architecto eius et quia temporibus harum ratione quisquam excepturi totam repellat," + 
 						" consequuntur assumenda debitis velit!", 
-						categorySeven));
+						categorySeven, tagTest));
 		Review reviewFourteen = reviewRepo.save(new Review("Receiving a Speeding Ticket", 1, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", 
 				"madlaw10", "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae cupiditate tempore deserunt odit" + 
 						" provident dolore architecto eius et quia temporibus harum ratione quisquam excepturi totam repellat," + 
 						" consequuntur assumenda debitis velit!", 
-						categorySeven));
+						categorySeven, tagTest));
 		
-		reviewTagRepo.save(new ReviewTag("thrilling", reviewOne, reviewEight, reviewEleven));
-		reviewTagRepo.save(new ReviewTag("free", reviewThree, reviewFour, reviewFive, reviewNine, reviewTen, reviewEleven, reviewTwelve, reviewFifteen));
-		reviewTagRepo.save(new ReviewTag("boring", reviewThree, reviewFive, reviewSix, reviewSeven, reviewThirteen));
-		reviewTagRepo.save(new ReviewTag("fun", reviewOne));
-		reviewTagRepo.save(new ReviewTag("time-consuming", reviewTwo, reviewFour, reviewFive, reviewSeven, reviewThirteen, reviewFifteen));
-		reviewTagRepo.save(new ReviewTag("expensive", reviewFourteen, reviewFifteen));
-		reviewTagRepo.save(new ReviewTag("stressful", reviewTwelve, reviewFourteen, reviewFifteen));
+//		reviewTagRepo.save(new ReviewTag("thrilling", reviewOne, reviewEight, reviewEleven));
+//		reviewTagRepo.save(new ReviewTag("free", reviewThree, reviewFour, reviewFive, reviewNine, reviewTen, reviewEleven, reviewTwelve, reviewFifteen));
+//		reviewTagRepo.save(new ReviewTag("boring", reviewThree, reviewFive, reviewSix, reviewSeven, reviewThirteen));
+//		reviewTagRepo.save(new ReviewTag("fun", reviewOne));
+//		reviewTagRepo.save(new ReviewTag("time-consuming", reviewTwo, reviewFour, reviewFive, reviewSeven, reviewThirteen, reviewFifteen));
+//		reviewTagRepo.save(new ReviewTag("expensive", reviewFourteen, reviewFifteen));
+//		reviewTagRepo.save(new ReviewTag("stressful", reviewTwelve, reviewFourteen, reviewFifteen));
+		//create review
+		Review reviewFarts = new Review("Crop Dusting", 5, "https://uccexpress.ie/wp-content/uploads/2018/10/jogging.jpg", "mateofrito", "Passing by somebody, or " +
+				" a group of people is fun and exciting.  I would highly recommend!", categorySeven, tagTest);
+		
+		//create review tag
+		ReviewTag tag1 = new ReviewTag("farts");
+		//create tag reference to the review
+		reviewFarts.getReviewTags().add(tag1);
+		//add reference in the tag
+		tag1.getReviews().add(reviewFarts);
+		//add the sumbitch to the repo
+		reviewRepo.save(reviewFarts);
+		
+	
+		
+		
+		
+		
 	}
 	
 
--- src/main/java/org/wecancodeit/userreviewsite/models/Review.java
+++ src/main/java/org/wecancodeit/userreviewsite/models/Review.java
@@ -1,10 +1,13 @@
 package org.wecancodeit.userreviewsite.models;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
@@ -26,6 +29,9 @@ public class Review {
 	private Category category;
 	@ManyToMany(mappedBy="reviews")
 	private Collection<ReviewTag> reviewTags;
+	@JoinTable(name = "review_tags",
+			  joinColumns = {@JoinColumn(name = "review_id", referencedColumnName = "id")},
+			  inverseJoinColumns = {@JoinColumn(name = "tag_id}", referencedColumnName = "id")})
 	@OneToMany(mappedBy="review")
 	private Collection<Comment> comments;
 
@@ -33,13 +39,14 @@ public class Review {
 	public Review() {
 	}
 
-	public Review(String title, int rating, String imageURL, String author, String content, Category category) {
+	public Review(String title, int rating, String imageURL, String author, String content, Category category, ReviewTag ...reviewTags) {
 		this.title = title;
 		this.rating = rating;
 		this.imageURL = imageURL;
 		this.author = author;
 		this.content = content;
 		this.category = category;
+		this.reviewTags = Arrays.asList(reviewTags);
 	}
 
 	public Long getId() {
